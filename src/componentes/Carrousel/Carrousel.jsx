cf742333dba1f32da67f03634562e2139fa3a790
Pedro Tissone <pedrotissone@gmail.com>
2023-07-03 05:24:28 -0300

    cree carpeta build para tratar de mejorar performance

--- src/componentes/Carrousel/Carrousel.jsx
+++ src/componentes/Carrousel/Carrousel.jsx
@@ -14,17 +14,16 @@ const Carrousel = () => {
       };
     
       return(
-        <>
-        {/* <center><h2 className="galeriaTitle">Happiness is only real when shared</h2></center>        */}
+        <>       
         <Slider {...settings}>
           <div>
-            <img className="carrouselImagenes" src="/img/carrousel1.jpg" alt="Tocado de flores en sombrero"/>
+            <img className="carrouselImagenes" src="/img/carrousel1.jpg" loading="lazy" alt="Tocado de flores en sombrero"/>
           </div>
           <div>
-          <img className="carrouselImagenes" src="/img/carrousel2.jpg" alt="Flores preservadas"/>
+          <img className="carrouselImagenes" src="/img/carrousel2.jpg" loading="lazy" alt="Flores preservadas"/>
           </div>
           <div>
-          <img className="carrouselImagenes" src="/img/carrousel3.jpg" alt="Niña con corona de flores"/>
+          <img className="carrouselImagenes" src="/img/carrousel3.jpg" loading="lazy" alt="Niña con corona de flores"/>
           </div>
         </Slider>
         </>         
